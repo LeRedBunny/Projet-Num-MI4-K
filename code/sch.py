b2ad6e576f9990752278b9b33cf4b61bded101a1
LeRedBunny <alexi.toutou22@gmail.com>
2026-06-17 14:51:50 +0200

    f

--- code/sch.py
+++ code/sch.py
@@ -1,10 +1,11 @@
 '''Approximates the wavefunction using Schrödinger's equation.'''
 
+from customTypes import WaveFunction, Interval, Function
 from derivative import derivative, derivative2
-from customTypes import WaveFunction, Interval
 from PaquetOndeGauss1d4k import GaussWP
 from numpy import empty, linspace
 from constants import H_BAR, ME
+import matplotlib.pyplot as plt
 
 
 def ApproximateNextPosition (position: int, wavefunction: WaveFunction, t_int: Interval, potential: float) -> None :
@@ -21,7 +22,7 @@ def ApproximateNextPosition (position: int, wavefunction: WaveFunction, t_int: I
         )
 
 
-def ApproximateWF (wavefunction: WaveFunction, x_int: Interval, t_int: Interval, potential: Interval) -> None :
+def ApproximateWaveFunction (wavefunction: WaveFunction, x_int: Interval, t_int: Interval, potential: Function) -> None :
     '''Completes the array representing the wavefunction using Schrödinger's equation.'''
     
     for i in range(len(x_int) - 1) :
@@ -38,7 +39,7 @@ if __name__ == '__main__' :
 
     # Space
     x_min = 0
-    x_max = 10
+    x_max = 1
     nx = 50
     x_int = linspace(x_min, x_max, nx)
 
@@ -48,8 +49,29 @@ if __name__ == '__main__' :
     nt = 50
     t_int = linspace(t_min, t_max, nt)
 
+    # Potential
+    potential = [0] * nx
+
     # Wavefunction
-    wavefunction = empty((nx, nt))
-    for i in nx :
-        wavefunction[0, i] = [GaussWP(k0, a, x, 0) for x in x_int]
+    wavefunction = empty((nx, nt), dtype=complex)
+    for i in range(nx) :
+        wavefunction[0, i] = GaussWP(k0, a, x_int[i], 0)
+    
+    ApproximateWaveFunction(wavefunction, x_int, t_int, potential)
+
+
+    # Display
+    fig, (ax_real, ax_imag) = plt.subplots(2)
+    fig.suptitle(f'Wavefunction Ψ at time t={t_min}s')
+
+    ax_real.plot(x_int, [wavefunction[i, 0].real for i in range(nx)])
+    ax_real.set_title(f'Real Part')
+
+    ax_imag.plot(x_int, [wavefunction[i, 0].imag for i in range(nx)])
+    ax_imag.set_title(f'Imaginary Part')
+
+
+    plt.show()
+
+
 
