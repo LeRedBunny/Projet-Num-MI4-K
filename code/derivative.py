4307d0d811ad2244adc09903b0581532cabcd5e9
LeRedBunny <alexi.toutou22@gmail.com>
2026-06-17 12:17:08 +0200

    b

--- code/derivative.py
+++ code/derivative.py
@@ -1,14 +1,16 @@
 '''Functions to approximate the first and second derivatives of a function'''
 
-def derivee (tab_f: list[float], tab_x: list[float]) -> list[float] :
-    '''Calcule la dérivée de f, renvoie un tableau vide si échec'''
+
+
+def derivative (tab_f: list[float], tab_x: list[float]) -> list[float] :
+    '''Approximates the derivative of f, returns an empty array if failed.'''
     if len(tab_f) != len(tab_x) :
         return []
     return [(tab_f[i + 1] - tab_f[i]) / (tab_x[i + 1] - tab_x[i]) for i in range(len(tab_f) - 1)]
 
 
-def derivee2 (tab_f: list[float], tab_d: list[float], tab_x: list[float]) -> list[float] :
-    '''Calcule la dérivée seconde de f, renvoie un tableau vide si échec'''
+def derivative2 (tab_f: list[float], tab_d: list[float], tab_x: list[float]) -> list[float] :
+    '''Approximates the second derivative of f, returns an empty array if failed.'''
     if len(tab_f) != len(tab_x) or len(tab_f) != len(tab_d) + 1 :
         return []
     return [(tab_f[i + 2] - 2 * tab_f[i + 1] + tab_f[i]) / (tab_x[i + 1] - tab_x[i]) ** 2 for i in range(len(tab_f) - 2)]
@@ -26,10 +28,10 @@ if __name__ == '__main__' :
     tab_f = [(x_0 + i * delta) ** 2 for i in range(n)]
     
     tab_derivee = [2 * x for x in tab_x[:-1]]
-    tab_approximation = derivee(tab_f, tab_x)
+    tab_approximation = derivative(tab_f, tab_x)
 
     tab_derivee2 = [2 for x in tab_x[:-2]]
-    tab_approximation2 = derivee2(tab_f, tab_approximation, tab_x)
+    tab_approximation2 = derivative2(tab_f, tab_approximation, tab_x)
 
     tab_erreur = [tab_approximation2[i] - tab_derivee2[i] for i in range(n - 2)]
 
--- code/sch.py
+++ code/sch.py
@@ -1,8 +1,30 @@
 '''Approximates the wavefunction using Schrödinger's equation.'''
 
+from derivative import derivative, derivative2
 from PaquetOndeGauss1d4k import GaussWP
 from numpy import empty, linspace
+from constants import H_BAR
 
+
+    # Types
+
+type WaveFunction = list[list[complex]]
+
+
+    # Function  
+
+def approximateInitialTime (wavefunction: WaveFunction) -> None :
+    '''Completes the first column of the array, corresponding to t0'''
+
+    wavefunction[0, :] = []
+
+
+def ApproximateWF (wavefunction: WaveFunction) -> None :
+    '''Completes the array representing the wavefunction using Schrödinger's equation.'''
+
+
+
+    # Main
 
 if __name__ == '__main__' :
 
@@ -13,16 +35,16 @@ if __name__ == '__main__' :
     # Space
     x_min = 0
     x_max = 10
-    x_splits = 50
-    x_int = linspace(x_min, x_max, x_splits)
+    nx = 50
+    x_int = linspace(x_min, x_max, nx)
 
     # Time
     t_min = 0
     t_max = 10
-    t_splits = 50
-    t_int = linspace(t_min, t_max, t_splits)
+    nt = 50
+    t_int = linspace(t_min, t_max, nt)
 
     # Wavefunction
-    wavefunction = empty((x_splits, t_splits))
-    wavefunction[0] = [GaussWP(k0, a, t, 0) for t in t_int]
+    wavefunction = empty((nx, nt))
+    wavefunction[0, :] = [GaussWP(k0, a, x, 0) for x in x_int]
 
